5abc0b34ad48df6d8d3bce8a4508961096c7591b
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-02 18:39:33 +0600

    user route updated

--- src/routes/user_route.js
+++ src/routes/user_route.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const {
+  registeUser,
+  getUser,
+  getPeoples,
+} = require("../modules/user/UserController");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json("hello world");
-});
+router.post("/auth/user/register", registeUser);
+router.get("/user", getUser);
+router.get("/peoples", getPeoples);
 
 module.exports = router;
