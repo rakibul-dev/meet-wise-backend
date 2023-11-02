38cf16f5e5e34b384bcd0c737106289c85e44a18
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-02 18:40:14 +0600

    conversation route pdated

--- src/routes/conversation_route.js
+++ src/routes/conversation_route.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const {
+  getConversations,
+  createConversation,
+} = require("../modules/conversation/ConversationController");
 const router = express.Router();
 
-router.get("/conversations",);
+router.get("/conversations", getConversations);
+
+router.post("/conversations", createConversation);
 
 module.exports = router;
